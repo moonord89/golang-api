a866b80ac97271d0b7c013886fb82a495b60bafd
moonord89 <48245450+moonord89@users.noreply.github.com>
2020-10-20 22:25:47 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,47 +1,32 @@
 package main
 
 import (
-	"crypto/tls"
-	"fmt"
+	"homepage"
+	"server"
 	"log"
 	"net/http"
-	"time"
+	"os"
+)
+
+var (
+	GcukCertFile    = os.Getenv("GCUK_CERT_FILE")
+	GcukKeyFile     = os.Getenv("GCUK_KEY_FILE")
+	GcukServiceAddr = os.Getenv("GCUK_SERVICE_ADDR")
 )
 
 func main() {
-	fmt.Print("Hello")
+	logger := log.New(os.Stdout, "gcuk ", log.LstdFlags|log.Lshortfile)
+
+	h := homepage.NewHandlers(logger)
+
 	mux := http.NewServeMux()
+	h.SetupRoutes(mux)
 
-	tlsConfig := &tls.Config{
-		// Causes servers to use Go's default ciphersuite preferences,
-		// which are tuned to avoid attacks. Does nothing on clients.
-		PreferServerCipherSuites: true,
-		// Only use curves which have assembly implementations
-		CurvePreferences: []tls.CurveID{
-			tls.CurveP256,
-			tls.X25519, // Go 1.8 only
-		},
-		MinVersion: tls.VersionTLS12,
-		CipherSuites: []uint16{
-			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305, // Go 1.8 only
-			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,   // Go 1.8 only
-			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+	srv := server.New(mux, GcukServiceAddr)
 
-			// Best disabled, as they don't provide Forward Secrecy,
-			// but might be necessary for some clients
-			// tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
-			// tls.TLS_RSA_WITH_AES_128_GCM_SHA256,
-		},
+	logger.Println("server starting")
+	err := srv.ListenAndServeTLS(GcukCertFile, GcukKeyFile)
+	if err != nil {
+		logger.Fatalf("server failed to start: %v", err)
 	}
-	srv := &http.Server{
-		Addr:         ":8080",
-		ReadTimeout:  5 * time.Second,
-		WriteTimeout: 5 * time.Second,
-		TLSConfig:    tlsConfig,
-		Handler:      mux,
-	}
-	go func() { log.Fatal(srv.ListenAndServe()) }()
 }
